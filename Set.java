852c7a50c7a6cf4df0cc87e9f00aa7f46bfd219f
Lachlan Imel <imell@CF165-20.cs.wwu.edu>
2022-04-28 14:50:11 -0700

    Wrote out member variables and method signatures for Set.java

---
+++ Set.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+import java.util.HashMap;
+
+public class Set {
+    // Tracks how many shots are left on the set
+    private int shotsLeft;
+    // HashMap tracks if there are players on any off-card roles, where the key is
+    // the number associated to the role, and the value is null if there are no
+    // players on that role, or the number representing the player on that role
+    private HashMap<Integer, Integer> offCardRoles;
+    // Every set has a scene card
+    private SceneCard scene;
+
+    // uses the HashMap of players, calculates their payout and calls setter to update player money/credits
+    private void offCardPayout(){
+    
+    }
+
+    private void removeSceneCard(){
+
+    }
+
+    public void setShotCounters(int shotsLeft){
+
+    }
+
+    private void sceneWrap(){
+        
+    }
+
+
+}
