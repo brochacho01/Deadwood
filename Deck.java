b812c869326573634f308060d3b896af974e8ccb
Lachlan Imel <imell@CF165-20.cs.wwu.edu>
2022-04-28 15:01:25 -0700

    Wrote out member variables and method signatures for Deck.java

---
+++ Deck.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private int deckSize;
+
+    private int cardsInDeck;
+
+    // By having all the SceneCards stored in an arrayList, their order is able to be randomized
+    private ArrayList<SceneCard> cards;
+
+    private void shuffle() {
+
+    }
+
+    // Creates an arrayList of SceneCards based on the number of players in the game
+    public ArrayList<SceneCard> createDeck(int numPlayers){
+        return cards;
+    }
+
+    // Deck deals a card to each set
+    public void dealCards() {
+
+    }
+
+
+}
