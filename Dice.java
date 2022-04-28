5fd8c98a244e016b8d53b14afab3cc2a4313eeb1
Lachlan Imel <imell@CF165-20.cs.wwu.edu>
2022-04-28 15:06:39 -0700

    Wrote out method signatures for Dice.java

---
+++ Dice.java
@@ -0,0 +1,13 @@
+public class Dice {
+    
+    // Calculates roll for acting, dependent upon players' rehearsal counters as a modifier
+    public int actRoll(int rehearsalCounters){
+        return 0;
+    }
+
+    // When a scene wraps, a number of dice is rolled equal to the budget, and since each individual roll must be preserved, they'll be stored in an array and returned
+    public int[] payoutRoll(int sceneBudget){
+        int[] returnArray = {0,0};
+        return returnArray;
+    }
+}
