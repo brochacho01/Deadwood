4748584085f68c8e6b07f5cbfb8e7e755ebc32a0
Lachlan Imel <imell@CF165-20.cs.wwu.edu>
2022-04-28 14:54:00 -0700

    Wrote out member variables and method signatures for SceneCard.java

---
+++ SceneCard.java
@@ -0,0 +1,14 @@
+public class SceneCard {
+    public int budget;
+    
+    // This HashMap works the same way as the one on set does, however this one is
+    // public so that set can access it to do calculations for sceneWrap
+    public HashMap<Integer, Integer> playersOnCard;
+    
+    public boolean isFlipped;
+
+    public void flip() {
+
+    }
+
+}
