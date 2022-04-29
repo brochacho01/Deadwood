17fbecd224ca263f5d7ee06f3eae471f4bd671d2
Ethan <81653895+YeeBurrito@users.noreply.github.com>
2022-04-28 17:29:21 -0700

    filled player class

---
+++ Player.java
@@ -0,0 +1,46 @@
+class Player
+{
+    private boolean hasRole;
+    private int rank;
+    private int location;
+    private int role;
+    private int balance;
+    private int credits;
+    private int rehearsalTokens;
+
+    //End the player's turn
+    public void endTurn()
+    {
+        return;
+    }
+
+    //Move the player
+    public void move(boolean hasRole, int location)
+    {
+        return;
+    }
+
+    //Take an available role
+    public void takeRole(boolean hasRole, int rank, int location)
+    {
+        return;
+    }
+
+    //Act in a given role
+    public void act(boolean hasRole, int role, int rehearsalTokens, int location)
+    {
+        return;
+    }
+
+    //Rehearse for a role
+    public void rehearse(int rehearsalTokens)
+    {
+        return;
+    }
+
+    //Upgrade a player's rank
+    public void upgrade(int location, int balance, int credits, int rank)
+    {
+        return;
+    }
+}
