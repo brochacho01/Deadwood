0e956727b51d5f227d4d7c8bdf45a04f6443c2fe
Ethan <81653895+YeeBurrito@users.noreply.github.com>
2022-04-28 17:20:19 -0700

    finished XMLParse class

---
+++ XMLParse.java
@@ -0,0 +1,18 @@
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.File;
+
+class XMLParse
+{
+    //parse the deck
+    public void parseDeck()
+    {
+        return;
+    }
+
+    //parse the board
+    public void parseBoard()
+    {
+        return;
+    }
+}
