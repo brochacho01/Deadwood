b2efafbd1d5e724855eef2674f390a224e822680
Ethan <81653895+YeeBurrito@users.noreply.github.com>
2022-05-01 13:30:45 -0700

    fixed board commit

--- Board.java
+++ Board.java
@@ -1,12 +1,11 @@
-class Board {
+class Board
+{
     private Set[] sets;
     private Player[] players;
     private int activeSets;
     private int playerTurn;
-    // Details how sections of the board connect to eachother
     private int[][] map;
 
-<<<<<<< HEAD
     //Setup the board
     public void setBoard()
     {
@@ -19,18 +18,6 @@ class Board {
         return;
     }
 
-    //Verify a move was valid
-    public boolean verifyMove(Player p, int[][] map)
-    {
-        return true;
-    }
-
-    //Verify a player has taken their turn correctly
-    public boolean verifyTurn(Player p)
-    {
-        return true;
-    }
-
     public void setDay(int day)
     {
         this.day = day;
@@ -40,17 +27,4 @@ class Board {
     {
         return players;
     }
-=======
-   
-    // Setup the board
-    public void setBoard() {
-        return;
-    }
-
-    // randomize the order of players
-    private void randomizePlayers(Player[] players) {
-        return;
-    }
-
->>>>>>> 85afe912744dd503dbf20e09e9ac887948f1ff85
 }
